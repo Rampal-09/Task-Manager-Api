59ac76960caffa421ac79c13010d275a2035fd2b
Rampal-09 <rampaltanwar0929@gmail.com>
2025-10-29 18:31:32 +0530

    Added roleMiddleware for role-based authorization

---
+++ task-manager-backend/middleware/roleMiddleware.js
@@ -0,0 +1,16 @@
+exports.roleMiddleware = (role) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not authorized" });
+    }
+    if (!role.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden - insufficient permissions",
+      });
+    }
+    next();
+  };
+};
